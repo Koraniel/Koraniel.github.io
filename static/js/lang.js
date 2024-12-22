204f9ebc615050fbcd1b8f17502711d60db960be
Lev Rymarev <leolewka@gmail.com>
2024-12-22 16:01:56 +0300

    Вроде бы исправить получилось, добавил частично смену языка на другие элементы. Мне кажется, что системный язык не определяется, буду проверять

--- static/js/lang.js
+++ static/js/lang.js
@@ -2,10 +2,30 @@ const translations = {
     en: {
       tab_header: "Koraniel",
       full_name: "Lev Rymarev",
+      occupation: "Student of AMI FCS HSE",
+      about_me_button: "About me in a nutshell",
+      job_exp_button: "My job experience",
+      partnership_button: "Partnership",
+      contact_me_button: "Contact me",
+      dark_light_theme_button: "Dark/White theme",
+      header1: "This is my site made for Deep Python course",
+      text1: "Я&nbsp;&mdash; студент Высшей Школы Экономики, ПМИ ФКН ВШЭ, <br> Учусь хорошо, мечтаю начать ходить на покру.",
+      contact_me_button2: "Contact me",
+
     },
     ru: {
       tab_header: "Кораниэль",
-      full_name: "Рымарев Лев"
+      full_name: "Рымарев Лев",
+      occupation: "Студент ПМИ ФКН ВШЭ",
+      about_me_button: "Вкратце обо мне",
+      job_exp_button: "Мой опыт работы",
+      partnership_button: "Сотрудничество",
+      contact_me_button: "Написать мне",
+      dark_light_theme_button: "Тёмная/Светлая тема",
+      header1: "Это сайт-визитка для ДЗ по углубленному питону",
+      text1: "Я&nbsp;&mdash; студент Высшей Школы Экономики, ПМИ ФКН ВШЭ, <br> Учусь хорошо, мечтаю начать ходить на покру.",
+      contact_me_button2: "Написать мне",
+      
     },
   };
   
